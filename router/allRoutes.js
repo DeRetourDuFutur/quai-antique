530f88f6b215f13ab8a758cb72d96b6d7e85276f
DeRetourDuFutur <antonymasson.dev@gmail.com>
2024-03-23 23:38:55 +0100

    Form Réservation

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -5,7 +5,12 @@ export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/Galerie", "Galerie", "/pages/galerie.html"),
   new Route("/Menus", "Nos Menus", "/pages/menus.html"),
-  new Route("/Reservation", "Réservation", "/pages/reservation.html"),
+  new Route(
+    "/AllResa",
+    "Vos réservations",
+    "/pages/reservations/all-resa.html"
+  ),
+  new Route("/Reserver", "Réserver", "/pages/reservations/new-resa.html"),
   new Route("/Compte", "Mon Compte", "/pages/auth/account.html"),
   new Route("/Sign-In", "Connexion", "/pages/auth/sign-in.html"),
   new Route("/Sign-Up", "Inscription", "/pages/auth/sign-up.html"),

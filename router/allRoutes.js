ca280283f3b74ffc81f241f0c0a681fc8ab27f06
DeRetourDuFutur <antonymasson.dev@gmail.com>
2024-03-23 18:41:54 +0100

    Ajout Galerie et Menus

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -3,9 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/Galerie", "La Galerie", "/pages/galerie.html"),
+  new Route("/Galerie", "Galerie", "/pages/galerie.html"),
+  new Route("/Menus", "Nos Menus", "/pages/menus.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-
-export const websiteName = "Le Quai Antique";
+export const websiteName = "Quai Antique";
